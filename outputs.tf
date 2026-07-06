6f57559a917dc37cc3dde6577832389c9d03b091
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:14 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,12 @@ output "arc_machine_automanage_configuration_assignments" {
   description = "All arc_machine_automanage_configuration_assignment resources"
   value       = azurerm_arc_machine_automanage_configuration_assignment.arc_machine_automanage_configuration_assignments
 }
+output "arc_machine_automanage_configuration_assignments_arc_machine_id" {
+  description = "List of arc_machine_id values across all arc_machine_automanage_configuration_assignments"
+  value       = [for k, v in azurerm_arc_machine_automanage_configuration_assignment.arc_machine_automanage_configuration_assignments : v.arc_machine_id]
+}
+output "arc_machine_automanage_configuration_assignments_configuration_id" {
+  description = "List of configuration_id values across all arc_machine_automanage_configuration_assignments"
+  value       = [for k, v in azurerm_arc_machine_automanage_configuration_assignment.arc_machine_automanage_configuration_assignments : v.configuration_id]
+}
 
