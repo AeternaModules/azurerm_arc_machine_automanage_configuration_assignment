32304c5967a596cccaf6ce3eb47e880c2e599713
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:58 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "arc_machine_automanage_configuration_assignments" {
-  description = "All arc_machine_automanage_configuration_assignment resources"
-  value       = azurerm_arc_machine_automanage_configuration_assignment.arc_machine_automanage_configuration_assignments
-}
 output "arc_machine_automanage_configuration_assignments_arc_machine_id" {
-  description = "List of arc_machine_id values across all arc_machine_automanage_configuration_assignments"
-  value       = [for k, v in azurerm_arc_machine_automanage_configuration_assignment.arc_machine_automanage_configuration_assignments : v.arc_machine_id]
+  description = "Map of arc_machine_id values across all arc_machine_automanage_configuration_assignments, keyed the same as var.arc_machine_automanage_configuration_assignments"
+  value       = { for k, v in azurerm_arc_machine_automanage_configuration_assignment.arc_machine_automanage_configuration_assignments : k => v.arc_machine_id }
 }
 output "arc_machine_automanage_configuration_assignments_configuration_id" {
-  description = "List of configuration_id values across all arc_machine_automanage_configuration_assignments"
-  value       = [for k, v in azurerm_arc_machine_automanage_configuration_assignment.arc_machine_automanage_configuration_assignments : v.configuration_id]
+  description = "Map of configuration_id values across all arc_machine_automanage_configuration_assignments, keyed the same as var.arc_machine_automanage_configuration_assignments"
+  value       = { for k, v in azurerm_arc_machine_automanage_configuration_assignment.arc_machine_automanage_configuration_assignments : k => v.configuration_id }
 }
 
